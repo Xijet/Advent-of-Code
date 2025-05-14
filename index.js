686c8676c89cd08d5c80fb1f536c4eee88816a39
a17towan <92051242+Xijet@users.noreply.github.com>
2025-05-14 20:19:49 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const reports = [
+  [7, 6, 4, 2, 1], // Safe
+  [1, 2, 7, 6, 8], // Unsafe
+  [9, 7, 6, 2, 1], // Unsafe
+  [1, 3, 2, 4, 5], // Unsafe
+  [8, 6, 4, 4, 1], // Unsafe
+  [1, 3, 6, 7, 9], // Safe
+];
+
+for (let i = 0; i < reports.length; i++) {
+  // Creating two loops to go through the 2d array
+let increasing = true;
+let decreasing = true;
+  for (let j = 1; j < reports[i].length; j++) {
+    let diff = reports[i][j] - reports[i][j - 1];
+    console.log("reports[" + i + "][" + j + "] = " + reports[i][j]);
+
+    if (diff >= 1 && diff <= 3) {
+      // Checks if the row is increasing within permitted range
+      decreasing = false;
+    } else if (diff <= -1 && diff >= -3) {
+      // Checks if the row is decreasing within permitted range
+      increasing = false;
+    } else {
+      increasing = false;
+      decreasing = false;
+    }
+  }
+
+  if (increasing) {
+    console.log("Report " + [i] + " is increasing (diff between 1 and 3). SAFE");
+  } else if (decreasing) {
+    console.log("Report " + [i] + " is decreasing (diff between 1 and 3). SAFE");
+  } else {
+    console.log("Report " + [i] + " is NOT strictly increasing or decreasing within range 1–3. NOT SAFE");
+  }
+}
